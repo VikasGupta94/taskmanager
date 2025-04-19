fd63fc951d17e9c9f3831f8ff7d4a72ee2b0ccb2
VikasGupta94 <vikas1766gupta@gmail.com>
2025-04-19 19:43:00 +0530

    changes

--- src/models/index.js
+++ src/models/index.js
@@ -1,41 +1,45 @@
 import fs from 'fs';
 import path from 'path';
 import { Sequelize, DataTypes } from 'sequelize';
-import { fileURLToPath } from 'url';
+import { fileURLToPath, pathToFileURL } from 'url';
 import serverConfig from '../config/server-config.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const basename=path.dirname(__filename);
+const basename = path.basename(__filename); 
+
 const db = {};
 
-let sequelize = new Sequelize(serverConfig.DATABASE_URL);
+//  Use DATABASE_URL from config
+const sequelize = new Sequelize(serverConfig.DATABASE_URL);
 
-  // sequelize = new Sequelize(config.database, config.username, config.password, config);
-
-
+//  Load all model files in the same directory
 const files = fs
   .readdirSync(__dirname)
   .filter(file => (
     file.indexOf('.') !== 0 &&
     file !== basename &&
-    file.slice(-3) === '.js' &&
-    file.indexOf('.test.js') === -1
+    file.endsWith('.js') &&
+    !file.endsWith('.test.js')
   ));
 
+//  Use file URL for dynamic import
 for (const file of files) {
-  const modelModule = await import(path.join(__dirname, file));
+  const fullPath = path.join(__dirname, file);
+  const modelModule = await import(pathToFileURL(fullPath)); 
   const model = modelModule.default(sequelize, DataTypes);
   db[model.name] = model;
 }
 
+//  Setup associations
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
 
+//  Export sequelize instance and all models
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-export default db ;
+export default db;
