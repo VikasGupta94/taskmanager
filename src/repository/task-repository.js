7a32af10a8914ea6a83d2d8cb6673ab47bf5a470
VikasGupta94 <vikas1766gupta@gmail.com>
2025-04-21 01:30:25 +0530

    changes

--- src/repository/task-repository.js
+++ src/repository/task-repository.js
@@ -4,4 +4,24 @@ module.exports = class TaskRepository extends CrudRepository{
     constructor(){
         super(task);
     }
+
+     async  softDeleteTaskById(id) {
+        const tempTask = await task.findByPk(id);
+        if (!tempTask || tempTask.isDeleted) {
+          return null; 
+        }
+        tempTask.isDeleted = true;
+        await tempTask.save();
+        return tempTask;
+      }
+
+      async  updateTaskById(id, updateData) {
+        const [updatedCount] = await task.update(updateData, {
+          where: {
+            id,
+            isDeleted: false 
+          }
+        });
+        return updatedCount > 0; 
+      }
 }
